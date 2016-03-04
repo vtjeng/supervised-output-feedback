1f0e716fa4195bb62bd0bae101c91f1832c89567
Vincent Tjeng <vtjeng@mit.edu>
2016-03-04 14:23:20 -0500

    Attempting to do some fancy stuff. Hopefully it will work!

--- tf_linear_regression.py
+++ tf_linear_regression.py
@@ -8,8 +8,8 @@ target_filename = 'small'
 X_data = np.genfromtxt(os.path.join(target_directory, target_filename+"_validationX.csv"), delimiter=',')
 Y_data = np.genfromtxt(os.path.join(target_directory, target_filename+"_validationY.csv"), delimiter=',')
 
-# X_subsample = X_data[0:10000]
-# Y_subsample = Y_data[0:10000]
+X_subsample = X_data[0:10000]
+Y_subsample = Y_data[0:10000]
 
 print('Succesfully imported dataset with of size {0}'.format(X_data.shape))
 
@@ -23,7 +23,7 @@ W = tf.Variable(tf.zeros([num_features, 1]))
 y_pred = tf.matmul(x, W)
 
 loss = tf.reduce_mean(tf.square(y_pred - y_)) # using least squares
-optimizer = tf.train.GradientDescentOptimizer(0.1)
+optimizer = tf.train.GradientDescentOptimizer(0.01)
 train_step = optimizer.minimize(loss)
 
 
@@ -34,6 +34,7 @@ sess.run(init)
 
 # Fit the line.
 for step in xrange(1000):
-    sess.run(train_step, feed_dict = {x: X_data, y_: Y_data}) # training on the full dataset
+    
+    sess.run(train_step, feed_dict = {x: X_subsample, y_: Y_subsample}) # training on the full dataset
     if step % 2 == 0:
         print(step, np.transpose(sess.run(W)))
